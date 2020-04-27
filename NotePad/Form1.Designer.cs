47595917dcf03b532a286faafa21563454695af6
Mara Rosa <edimara.rosa@hotmail.com>
2020-04-26 23:51:00 -0300

    add dialogue to select files

--- NotePad/Form1.Designer.cs
+++ NotePad/Form1.Designer.cs
@@ -118,6 +118,9 @@
             // 
             // richTextBox1
             // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.richTextBox1.Location = new System.Drawing.Point(0, 41);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.Size = new System.Drawing.Size(771, 350);
@@ -133,7 +136,8 @@
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Simple Notepad";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
--- NotePad/Form1.cs
+++ NotePad/Form1.cs
@@ -12,9 +12,18 @@ namespace NotePad
 {
     public partial class Form1 : Form
     {
+        //Dialogs
+        private OpenFileDialog openFileDialog;
+        private SaveFileDialog saveFileDialog;
+        private FontConverter fontDialog;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
